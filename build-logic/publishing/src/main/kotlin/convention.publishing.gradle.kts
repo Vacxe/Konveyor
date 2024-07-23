4baa5d092cd2bb3bde416fa26bebd5b03b1d6ef7
Konstantin Aksenov <aksenov.kostya@gmail.com>
2024-07-23 21:49:22 +1000

    feat(core): new release convention plugin

--- build-logic/publishing/src/main/kotlin/convention.publishing.gradle.kts
+++ build-logic/publishing/src/main/kotlin/convention.publishing.gradle.kts
@@ -1,8 +1,19 @@
-import java.net.URI
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
+    id("java")
     id("maven-publish")
     id("signing")
+    id("com.github.johnrengelman.shadow")
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+tasks.withType<ShadowJar>().configureEach {
+    archiveClassifier.set("")
 }
 
 val releaseMode: String? by project
@@ -11,28 +22,26 @@ val versionSuffix = when (releaseMode) {
     else -> "-SNAPSHOT"
 }
 
-group = "io.github.vacxe"
 version = readVersion() + versionSuffix
 
-configure<PublishingExtension> {
-    publications {
-        create<MavenPublication>("default") {
-            groupId = project.group.toString()
+fun readVersion(): String {
+    return project.layout.projectDirectory.file("version").asFile.readText().trim()
+}
 
-            components.whenObjectAdded {
-                if (this.name == "release") {
-                    from(components["release"])
-                }
-            }
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = "konveyor"
+
+            from(components["java"])
 
             pom {
-                name.set("konveyor")
-                url.set("https://github.com/vacxe/konveyor")
+                name.set("Konveyor")
                 description.set("Random data class generator")
-
+                url.set("https://github.com/vacxe/konveyor")
                 licenses {
                     license {
-                        name.set("The Apache License, Version 2.0")
+                        name.set("The Apache Software License, Version 2.0")
                         url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
                     }
                 }
@@ -44,33 +53,28 @@ configure<PublishingExtension> {
                     }
                 }
                 scm {
-                    url.set("https://github.com/KakaoCup/Kakao.git")
-                    connection.set("scm:git:ssh://github.com/vacxe/konveyor")
-                    developerConnection.set("scm:git:ssh://github.com/vacxe/konveyor")
+                    connection.set("scm:git:github.com/vacxe/konveyor.git")
+                    developerConnection.set("scm:git:ssh://github.com/vacxe/konveyor.git")
+                    url.set("https://github.com/vacxe/konveyor")
                 }
             }
         }
-    }
-    repositories {
-        maven {
-            name = "Local"
-            setUrl("${project.rootDir}/build/repository")
-        }
-        maven {
-            name = "OSSHR"
-            credentials {
-                username = System.getenv("SONATYPE_USERNAME")
-                password = System.getenv("SONATYPE_PASSWORD")
-            }
-            url = URI.create(
-                when (releaseMode) {
+        repositories {
+            maven {
+                credentials {
+                    username = System.getenv("SONATYPE_USERNAME")
+                    password = System.getenv("SONATYPE_PASSWORD")
+                }
+                name = "Sonatype"
+
+                setUrl(when (releaseMode) {
                     "RELEASE" -> System.getenv("SONATYPE_RELEASES_URL")
                         ?: "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
 
                     else -> System.getenv("SONATYPE_SNAPSHOTS_URL")
                         ?: "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-                }
-            )
+                })
+            }
         }
     }
 }
@@ -80,14 +84,10 @@ val keyId: String? = System.getenv("GPG_KEY_ID")
 
 if (!passphrase.isNullOrBlank() && !keyId.isNullOrBlank()) {
     project.configure<SigningExtension> {
-        sign(publishing.publications.getByName("default"))
+        sign(publishing.publications)
     }
 
     project.extra.set("signing.keyId", keyId)
     project.extra.set("signing.password", passphrase)
     project.extra.set("signing.secretKeyRingFile", "${project.rootProject.rootDir}/buildsystem/secring.gpg")
-}
-
-fun readVersion(): String {
-    return project.layout.projectDirectory.file("version").asFile.readText().trim()
 }
